36a05587c09f81f477bf665eb16bf6486f32d17a
Julian Rivera <julianchos.rivera@gmail.com>
2025-01-15 19:28:36 -0500

    feat: enable routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
+import Router from "./TravelCore/Routes"
+import { BrowserRouter } from "react-router-dom";
 
-import { Header } from './TravelCore/Components/Header';
-import { Hero } from './TravelCore/Components/Hero';
-import { Steps } from './TravelCore/Components/Steps';
-import { Features } from './TravelCore/Components/Features';
-import { Stats } from './TravelCore/Components/Stats';
-import { Footer } from './TravelCore/Components/Footer';
-import { WhatsAppButton } from './TravelCore/Components/WhatsAppButton';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Header />
-      <Hero />
-      <Steps />
-      <Features />
-      <Stats />
-      <Footer />
-      <WhatsAppButton />
-    </div>
+    <>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    </>
   );
 }
 
--- src/TravelCore/Routes/index.tsx
+++ src/TravelCore/Routes/index.tsx
@@ -1,33 +1,22 @@
-import React, { Suspense, lazy, ComponentType } from 'react';
+import React, { lazy } from 'react';
 import { useRoutes, RouteObject } from 'react-router-dom';
-import SuspenseLoader from '../Components/Epic/SuspenseLoader';
+//import SuspenseLoader from '../Components/Epic/SuspenseLoader';
 import Layout from '../Styles/Layout';
 
-const Loader = <P extends object>(Component: React.LazyExoticComponent<ComponentType<P>>) => {
+/*const Loader = <P extends object>(Component: React.LazyExoticComponent<ComponentType<P>>) => {
     return (props: P) => (
         <Suspense fallback={<SuspenseLoader />}>
             <Component {...props} />
         </Suspense>
     );
-};
+};*/
 
-const ProductsPage = Loader(
-    lazy(() => import('../../TravelFeatures/Home/pages/productsPage.tsx'))
-);
-const HomeViewPage = Loader(
-    lazy(() => import('../../TravelFeatures/Home/pages/homeFlowPage.tsx'))
-);
-const TripQuotePage = Loader(
-    lazy(() => import('../../TravelFeatures/TripQuote/pages/tripQuotePage.tsx'))
-);
-const CommsQuotePage = Loader(
-    lazy(() => import('../../TravelFeatures/CommsQuote/pages/commsQuotePage.tsx'))
-);
-const InvoicePage = Loader(
-    lazy(() => import('../../TravelFeatures/Invoice/pages/invoicePage.tsx'))
-);
+const ProductsPage = lazy(() => import('../../TravelFeatures/Home/pages/productsPage.tsx'));
+const HomeViewPage = lazy(() => import('../../TravelFeatures/Home/pages/homeFlowPage.tsx'));
+const TripQuotePage = lazy(() => import('../../TravelFeatures/TripQuote/pages/tripQuotePage.tsx'));
+const CommsQuotePage = lazy(() => import('../../TravelFeatures/CommsQuote/pages/commsQuotePage.tsx'));
+const InvoicePage = lazy(() => import('../../TravelFeatures/Invoice/pages/invoicePage.tsx'));
 
-// Definición de rutas tipadas
 export default function Router(): React.ReactElement | null {
     const routes: RouteObject[] = [
         {
