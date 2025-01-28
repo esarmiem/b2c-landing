cf0c0ebb25158c02fe1fb2ca7417f08a1b0091e1
elder <dev.alaskatech@gmail.com>
2025-01-28 11:50:27 -0500

    Add HeroCarousel component in homFlowPage

--- src/TravelFeatures/Home/pages/homeFlowPage.tsx
+++ src/TravelFeatures/Home/pages/homeFlowPage.tsx
@@ -6,6 +6,7 @@ import { Certifications } from "../../../TravelCore/Components/Epic/productsPage
 import { Features } from "../../../TravelCore/Components/Epic/productsPage/Features";
 import { Testimonials } from "../../../TravelCore/Components/Epic/productsPage/Testimonials";
 import { Stats } from "../../../TravelCore/Components/Epic/productsPage/Stats";
+import { HeroCarousel } from "../../../TravelCore/Components/Epic/homeFlowPage/HeroCarousel";
 
 // Definir tipos para los posibles resultados de las respuestas de autenticación y masters
 /*interface AuthResponse {
@@ -56,6 +57,7 @@ const HomeViewPage: React.FC = () => {
 
   return (
     <>
+      <HeroCarousel />
       <Certifications />
       <Features />
       <Testimonials />
