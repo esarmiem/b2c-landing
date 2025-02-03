d7189b1a7db6caed08c0a22d7b5937c2f0c96df2
Julian Rivera <julianchos.rivera@gmail.com>
2025-02-03 11:18:31 -0500

    feat: adding persisted the languaje user selected

--- src/config/i18next.config.ts
+++ src/config/i18next.config.ts
@@ -1,7 +1,18 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend"
 import {initReactI18next} from "react-i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 
-i18n.use(Backend).use(initReactI18next).init({
-    fallbackLng: 'es'
+
+i18n.use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next).init({
+    fallbackLng: 'es',
+    interpolation: {
+        escapeValue: false,
+    },
+    detection: {
+        order: ["localStorage", "cookie", "navigator", "htmlTag"],
+        caches: ["localStorage", "cookie"], // Guarda el idioma en localStorage y cookies
+    }
 })
