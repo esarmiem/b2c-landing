48e5522d24e59dd99cfa517754933260aceaca9e
rafatoski <rafatoski@hotmail.com>
2025-02-10 21:01:12 -0500

    swipe grid and list
    added interaction between grid and list on products page

--- src/TravelCore/Components/Epic/CardProduct.tsx
+++ src/TravelCore/Components/Epic/CardProduct.tsx
@@ -9,11 +9,49 @@ interface CardProductProps {
   typeOfProduct: string;
   details: string[];
   recommended?: boolean;
+  viewType?: 'list' | 'grid';
 }
 
-const CardProduct: React.FC<CardProductProps> = ({ title, subtitle, price, originalPrice, details, typeOfProduct, recommended }) => {
+const CardProduct: React.FC<CardProductProps> = ({ title, subtitle, price, originalPrice, details, typeOfProduct, recommended, viewType = 'grid', }) => {
+  if (viewType === 'list') {
+    return (
+      <div className="flex border-2 border-neutral-800 rounded-lg overflow-hidden">
+        {/* Sección izquierda: Información resumida */}
+        <div className={`w-1/3 flex flex-col items-center justify-center p-4 ${recommended ? 'bg-red-800 text-white' : 'bg-zinc-100 text-neutral-800'}`}>
+          <h2 className="font-bold text-lg">{title}</h2>
+          {recommended && (
+            <div className="mt-2 text-xs font-bold bg-stone-800 text-white px-2 py-1 rounded">
+              Recomendado
+            </div>
+          )}
+        </div>
+        {/* Sección derecha: Detalles */}
+        <div className="w-2/3 p-4 flex flex-col justify-between">
+          <div>
+            <p className={`font-bold ${recommended ? 'text-red-100' : 'text-neutral-800'}`}>{subtitle}</p>
+            <p className="mt-1 text-xl font-bold">{price}</p>
+            <p className="line-through text-sm">{originalPrice}</p>
+            <ul className="mt-2 space-y-1">
+              {details.map((detail, idx) => (
+                <li key={idx} className="flex items-center gap-1 text-sm">
+                  <CircleCheck className="w-3 h-3 text-green-500" />
+                  {detail}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="mt-4">
+            <button className="w-full bg-red-500 text-white py-2 px-4 rounded-full font-bold  hover:bg-red-700 transition-all">
+              Seleccionar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="max-w-[320px] rounded-3xl border-2 border-neutral-800 relative overflow-hidden">
+    <div className="rounded-3xl border-2 border-neutral-800 relative overflow-hidden">
       <div className={` text-center px-1 py-8 ${recommended ? 'bg-red-800 text-white' : 'bg-zinc-100 text-neutral-800'}`}>
         {recommended && (
           <div className="text-white font-bold text-sm py-1 px-3 bg-stone-800 rounded-lg absolute top-0 left-1/2 transform -translate-x-1/2">
@@ -36,7 +74,7 @@ const CardProduct: React.FC<CardProductProps> = ({ title, subtitle, price, origi
       <div className="p-3 space-y-3">
         <ul className="text-sm text-gray-600 mb-3">
           {details.map((detail, idx) => (
-            <li key={idx} className="flex items-top mb-2 gap-1 font-bold">
+            <li key={idx} className="flex items-start mb-2 gap-1 font-bold">
               <CircleCheck className="h-3 w-3 text-green-500 mt-1 flex-shrink-0" />
               {detail}
             </li>
@@ -44,7 +82,7 @@ const CardProduct: React.FC<CardProductProps> = ({ title, subtitle, price, origi
         </ul>
       </div>
       <div className="p-3 space-y-1 text-center">
-        <button className="bg-red-500 text-white py-3 px-4 rounded-full w-full font-bold">
+        <button className="bg-red-500 text-white py-3 px-4 rounded-full w-full font-bold hover:bg-red-700 transition-all">
           Seleccionar
         </button>
         <a href="#" className="text-xs text-blue-600 hover:underline">
--- src/TravelCore/Components/Epic/ProductsRow.tsx
+++ src/TravelCore/Components/Epic/ProductsRow.tsx
@@ -1,77 +1,87 @@
 import React from 'react';
 import CardProduct from './CardProduct';
 
-const ProductsRow: React.FC = () => {
-    const cardData = [
-        {
-            title: 'DISCOVER',
-            subtitle: 'Protección básica 22.000 USD',
-            price: 'US$ 22.000',
-            originalPrice: 'US$ 30.000',
-            typeOfProduct: 'Ideal para: VACACIONES',
-            details: [
-                'Asistencia médica por accidente USD 35,000/EUR 35,000',
-                'Cancelación e interrupción Multicausa de viaje contratado',
-                'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
-                'Cobertura deportes amateur USD 1,000.00',
-                'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
-            ],
-        },
-        {
-            title: 'DISCOVER',
-            subtitle: 'Protección standard 35.000 USD',
-            price: 'US$ 35.000',
-            originalPrice: 'US$ 50.000',
-            typeOfProduct: 'Ideal para: VACACIONES',
-            details: [
-                'Asistencia médica por accidente USD 35,000/EUR 35,000',
-                'Cancelación e interrupción Multicausa de viaje contratado',
-                'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
-                'Cobertura deportes amateur USD 1,000.00',
-                'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
-            ],
-            recommended: true
-        },
-        {
-            title: 'DISCOVER',
-            subtitle: 'Protección especial 55.000 USD',
-            price: 'US$ 55.000',
-            originalPrice: 'US$ 75.000',
-            typeOfProduct: 'Ideal para: VACACIONES',
-            details: [
-                'Asistencia médica por accidente USD 35,000/EUR 35,000',
-                'Cancelación e interrupción Multicausa de viaje contratado',
-                'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
-                'Cobertura deportes amateur USD 1,000.00',
-                'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
-            ],
-        },
-        {
-            title: 'DISCOVER',
-            subtitle: 'Protección ideal 110.000 USD',
-            price: 'US$ 110.000',
-            originalPrice: 'US$ 150.000',
-            typeOfProduct: 'Ideal para: VACACIONES',
-            details: [
-                'Asistencia médica por accidente USD 35,000/EUR 35,000',
-                'Cancelación e interrupción Multicausa de viaje contratado',
-                'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
-                'Cobertura deportes amateur USD 1,000.00',
-                'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
-            ],
-        },
-    ];
+interface ProductsRowProps {
+  viewType: 'list' | 'grid';
+}
 
-    return (
-        <div className="max-w-6xl mx-auto p-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
-                {cardData.map((card, index) => (
-                    <CardProduct key={index} {...card} />
-                ))}
-            </div>
+const ProductsRow: React.FC<ProductsRowProps> = ({ viewType }) => {
+  const cardData = [
+    {
+      title: 'DISCOVER',
+      subtitle: 'Protección básica 22.000 USD',
+      price: 'US$ 22.000',
+      originalPrice: 'US$ 30.000',
+      typeOfProduct: 'Ideal para: VACACIONES',
+      details: [
+        'Asistencia médica por accidente USD 35,000/EUR 35,000',
+        'Cancelación e interrupción Multicausa de viaje contratado',
+        'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
+        'Cobertura deportes amateur USD 1,000.00',
+        'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
+      ],
+    },
+    {
+      title: 'DISCOVER',
+      subtitle: 'Protección standard 35.000 USD',
+      price: 'US$ 35.000',
+      originalPrice: 'US$ 50.000',
+      typeOfProduct: 'Ideal para: VACACIONES',
+      details: [
+        'Asistencia médica por accidente USD 35,000/EUR 35,000',
+        'Cancelación e interrupción Multicausa de viaje contratado',
+        'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
+        'Cobertura deportes amateur USD 1,000.00',
+        'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
+      ],
+      recommended: true
+    },
+    {
+      title: 'DISCOVER',
+      subtitle: 'Protección especial 55.000 USD',
+      price: 'US$ 55.000',
+      originalPrice: 'US$ 75.000',
+      typeOfProduct: 'Ideal para: VACACIONES',
+      details: [
+        'Asistencia médica por accidente USD 35,000/EUR 35,000',
+        'Cancelación e interrupción Multicausa de viaje contratado',
+        'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
+        'Cobertura deportes amateur USD 1,000.00',
+        'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
+      ],
+    },
+    {
+      title: 'DISCOVER',
+      subtitle: 'Protección ideal 110.000 USD',
+      price: 'US$ 110.000',
+      originalPrice: 'US$ 150.000',
+      typeOfProduct: 'Ideal para: VACACIONES',
+      details: [
+        'Asistencia médica por accidente USD 35,000/EUR 35,000',
+        'Cancelación e interrupción Multicausa de viaje contratado',
+        'Robo o Pérdida de pasaporte exclusivamente en viaje USD 70.00',
+        'Cobertura deportes amateur USD 1,000.00',
+        'Demora de vuelo = Sala VIP (A partir de 60 minutos) Incluido'
+      ],
+    },
+  ];
 
-        </div>
-    );
+  // Definimos las clases condicionales:
+  // Si viewType es "list", usamos una grilla de 1 columna; si es "grid", usamos varias columnas.
+  const containerClasses =
+    viewType === 'list'
+      ? 'grid grid-cols-1 gap-4'
+      : 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center';
+
+  return (
+    <div className="max-w-6xl mx-auto p-4">
+      <div className={containerClasses}>
+        {cardData.map((card, index) => (
+          <CardProduct key={index} viewType={viewType} {...card} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductsRow;
