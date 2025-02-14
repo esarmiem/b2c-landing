e1053cb1ab27be92254c237fc64791d65b199f65
elder <dev.alaskatech@gmail.com>
2025-02-13 22:47:59 -0500

    ADD LoadingFormScreen, FIX HeroCarousel, TravelButtonForm, WhatsAppButton, and homePage components

--- src/TravelCore/Components/Epic/HeroCarousel.tsx
+++ src/TravelCore/Components/Epic/HeroCarousel.tsx
@@ -61,9 +61,10 @@ export const HeroCarousel: React.FC<CarouselProps> = ({
         className="absolute top-1/2 -translate-y-1/2 start-0 z-30 flex items-center justify-center h-12 px-4 cursor-pointer group focus:outline-none"
         onClick={handlePrevious}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full "> 
+        {/* bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none */}
           <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            className="w-4 h-4 text-gray-100 dark:text-gray-800 rtl:rotate-180"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -87,9 +88,9 @@ export const HeroCarousel: React.FC<CarouselProps> = ({
         className="absolute top-1/2 -translate-y-1/2 end-0 z-30 flex items-center justify-center h-12 px-4 cursor-pointer group focus:outline-none"
         onClick={handleNext}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full">
           <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            className="w-4 h-4 text-gray-100 dark:text-gray-800 rtl:rotate-180"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
