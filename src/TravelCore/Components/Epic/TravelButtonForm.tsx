2714e5b35c7ee251c498a052a290d781446c4d98
Brayan Mercado <brayan@belvi.co>
2025-02-17 10:30:32 -0500

    Travel Button animation

--- src/TravelCore/Components/Epic/TravelButtonForm.tsx
+++ src/TravelCore/Components/Epic/TravelButtonForm.tsx
@@ -1,22 +1,24 @@
-import { MouseEvent } from 'react';
-import { Button } from '@/components/ui/button'; // Adjust import path as needed
-import {useTranslation} from "react-i18next"; // Your loading screen component
-import { Search } from 'lucide-react';
+import {MouseEvent} from 'react'
+import {Button} from '@/components/ui/button'
+import {useTranslation} from "react-i18next"
+import {Search} from 'lucide-react'
 
 interface TravelButtonFormProps {
-  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void
 }
 
 export const TravelButtonForm = ({onClick}: TravelButtonFormProps) => {
   const { t } = useTranslation(["home"])
 
   return (
-      <Button 
-        className="bg-red-600 hover:bg-black rounded-full lg:w-full h-auto"
+      <Button
+        className="relative overflow-hidden border border-red-600 bg-red-600 text-white shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-red-600 hover:before:w-2/4 hover:before:bg-black hover:after:w-2/4 hover:after:bg-black rounded-full lg:w-full h-auto px-4 py-2 flex items-center justify-center gap-2"
         onClick={onClick}
       >
+        <span className="relative z-10 flex items-center gap-2">
         <Search />
         {t('label-button-search')}
+        </span>
       </Button>
-  );
-};
+  )
+}
