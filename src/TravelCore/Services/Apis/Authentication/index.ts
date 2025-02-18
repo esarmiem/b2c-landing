ce54cdfb84d2ebc45bdbd1b1e8b3eafd105fca40
fdmp2 <faritmajul@gmail.com>
2025-02-18 17:55:04 -0500

    test

--- src/TravelCore/Services/Apis/Authentication/index.ts
+++ src/TravelCore/Services/Apis/Authentication/index.ts
@@ -1,67 +1,71 @@
-import {axiosHttp} from "../../../Utils/http.ts"
-import {SERVICE_AUTHENTICATION, USER_NAME, PASSWORD} from "../../../Utils/constants.ts"
+import { axiosHttp } from "../../../Utils/http.ts";
+import {
+  SERVICE_AUTHENTICATION,
+  USER_NAME,
+  PASSWORD,
+} from "../../../Utils/constants.ts";
 
 interface Permissions {
-  idPermission: number
-  name: string
-  description: string
-  estaActivo: boolean
+  idPermission: number;
+  name: string;
+  description: string;
+  estaActivo: boolean;
 }
 
 interface PermissionsRoles {
-  idPermissionRole: number
-  idPermission: number
-  idRole: number
-  permission: Permissions
+  idPermissionRole: number;
+  idPermission: number;
+  idRole: number;
+  permission: Permissions;
 }
 
 interface Roles {
-  idRole: number
-  name: string
-  description: string
-  estaActivo: boolean
-  permissionsRoles: PermissionsRoles[]
+  idRole: number;
+  name: string;
+  description: string;
+  estaActivo: boolean;
+  permissionsRoles: PermissionsRoles[];
 }
 
 interface Users {
-  idUser: number
-  firstName: string
-  secondName: string
-  lastName: string
-  secondLastName: string
-  documentNumber: string
-  email: string
-  phone: string
-  userName: string
-  isActive: boolean
-  remember_token: string | null
-  idRole: number
-  idAgencia: number | null
-  role: Roles
-  agencia: string | null
+  idUser: number;
+  firstName: string;
+  secondName: string;
+  lastName: string;
+  secondLastName: string;
+  documentNumber: string;
+  email: string;
+  phone: string;
+  userName: string;
+  isActive: boolean;
+  remember_token: string | null;
+  idRole: number;
+  idAgencia: number | null;
+  role: Roles;
+  agencia: string | null;
 }
 
 export interface AuthData {
-  user: Users
+  user: Users;
   payload: {
-    accessToken: string
-    refreshToken: string
-  }
+    accessToken: string;
+    refreshToken: string;
+  };
 }
 
 interface ApiResponse {
-  data: AuthData
-  error: string | null
+  data: AuthData;
+  error: string | null;
 }
 
 export const AUTH_API = {
   login: (): Promise<ApiResponse> => {
-    const loginData = {username: USER_NAME, password: PASSWORD}
+    const loginData = { username: USER_NAME, password: PASSWORD };
     return axiosHttp({
       path: `${SERVICE_AUTHENTICATION}`,
-      method: 'POST',
+      method: "POST",
       data: loginData,
-      session: null
-    })
+      session: null,
+    });
   },
-}
+};
